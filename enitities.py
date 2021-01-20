c8a7920f3c222f7a35837c5f27fcdf396da5bacd
Paul <paulwjaroslawski@gmail.com>
2021-01-20 13:16:34 -0500

    Finished project

--- enitities.py
+++ enitities.py
@@ -3,6 +3,7 @@ import pygame
 pygame.init()
 
 spd = 0.1
+score = 0
 
 
 class Entity:
@@ -26,14 +27,14 @@ class Player(Entity):
         self.right = 0
         self.bullets = []
 
-    def entity_state(self, _screen, _width):
+    def entity_state(self, _screen, _width, _enemy_list, _explosion):
         self.draw_entity(_screen)
         self.moving(_width)
 
         if len(self.bullets) != 0:
             for bullet in self.bullets:
                 if bullet.y_pos > 10:
-                    bullet.move(_screen)
+                    bullet.move(_screen, _enemy_list, _explosion)
                 else:
                     self.bullets.pop(self.bullets.index(bullet))
 
@@ -46,7 +47,7 @@ class Player(Entity):
         self.x_pos += axis * spd
 
     def fire(self):
-        bullet = Bullet(self.x_pos, self.y_pos)
+        bullet = Bullet(self.x_pos + 16, self.y_pos)
         self.bullets.append(bullet)
 
 
@@ -65,8 +66,9 @@ class Enemy(Entity):
             self.dir = -1
             self.y_pos += 32
         elif self.x_pos + _spd <= 0:
-            self.dir = 1
-            self.y_pos += 32
+            if self.y_pos > 32:
+                self.dir = 1
+                self.y_pos += 32
 
         self.x_pos += _spd
 
@@ -77,7 +79,18 @@ class Bullet:
         self.x_pos = x
         self.y_pos = y
 
-    def move(self, _screen):
+    def move(self, _screen, _enemy_list, _explosion):
         _screen.blit(self.img, (self.x_pos, self.y_pos))
+        self.enemy_collision(_enemy_list, _explosion)
         self.y_pos -= 0.5
+
+    def enemy_collision(self, enemy_list, _explosion):
+        global score
+        for enemy in enemy_list:
+            if enemy.x_pos < self.x_pos < (enemy.x_pos + 64) and enemy.y_pos < self.y_pos < enemy.y_pos + 64:
+                enemy_list.pop(enemy_list.index(enemy))
+                _explosion()
+                score += 1
+                print(score)
+
 
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+from pygame import mixer
+import enitities
 from enitities import Player, Enemy
 
 # Initialize Pygame
@@ -7,6 +9,26 @@ pygame.init()
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SPACE_BG = (40, 36, 53)
+FONT = pygame.font.Font('SourceCodePro-Regular.ttf', 24)
+mixer.music.load('through space.ogg')
+mixer.music.play(-1)
+
+fire_fx = mixer.Sound('gun.wav')
+explosion_fx = mixer.Sound('explode.wav')
+
+
+def explosion():
+    explosion_fx.play()
+
+
+def show_score(x, y, _score):
+    score_display = FONT.render(f"Score: {_score}", True, (255, 255, 255))
+    screen.blit(score_display, (x, y))
+
+
+level_started = False
+enemies = []
+enemy_queue = 3
 
 player_img = 'player_ship.png'
 enemy_img = 'Alien.png'
@@ -18,7 +40,7 @@ player_y = 480
 player = Player(player_x, player_y, player_sprite)
 
 enemy_sprite = pygame.image.load(enemy_img)
-enemy = Enemy(SCREEN_WIDTH/2 - 32, 32, enemy_sprite)
+
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 icon = pygame.image.load('alien_sml.png')
@@ -28,6 +50,13 @@ pygame.display.set_caption('Space Invaders')
 running = True
 
 while running:
+
+    score = enitities.score
+    if not level_started:
+        for i in range(enemy_queue):
+            enemies.append(Enemy(-96 * i, 32, enemy_sprite))
+        level_started = True
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -38,6 +67,7 @@ while running:
                 player.right = 1
             elif event.key == pygame.K_SPACE:
                 player.fire()
+                fire_fx.play()
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_a:
                 player.left = 0
@@ -45,9 +75,10 @@ while running:
                 player.right = 0
     
     screen.fill(SPACE_BG)
-    player.entity_state(screen, SCREEN_WIDTH)
-    enemy.entity_state(screen, SCREEN_WIDTH)
+    show_score(600, 32, score)
+    player.entity_state(screen, SCREEN_WIDTH, enemies, explosion)
+    if len(enemies) != 0:
+        for enemy in enemies:
+            enemy.entity_state(screen, SCREEN_WIDTH)
 
-
-    
     pygame.display.update()
